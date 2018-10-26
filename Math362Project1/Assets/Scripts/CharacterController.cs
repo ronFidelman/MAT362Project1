782ca5860b18e3091dcd85863c0e34914a7c9494
Ron Fidelman <ron.fidelman@digipen.edu>
2018-10-26 15:13:51 -0700

    Added the project.

---
+++ Math362Project1/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterController : MonoBehaviour {
+
+  public KeyCode Up = KeyCode.UpArrow;
+  public KeyCode Down = KeyCode.DownArrow;
+  public KeyCode Left = KeyCode.LeftArrow;
+  public KeyCode Right = KeyCode.RightArrow;
+
+  public float MoveSpeed = 5;
+  public float MaxSpeed = 20;
+
+  Rigidbody rb;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void Update () {
+    Vector3 movementForce = new Vector3();
+
+    if(Input.GetKey(Up))
+    {
+      movementForce.y += 1;
+    }
+    if (Input.GetKey(Down))
+    {
+      movementForce.y += -1;
+    }
+
+    if (Input.GetKey(Right))
+    {
+      movementForce.x += 1;
+    }
+    if (Input.GetKey(Left))
+    {
+      movementForce.x += -1;
+    }
+    movementForce = movementForce.normalized * MoveSpeed;
+
+    rb.AddForce(movementForce);
+
+    if(rb.velocity.sqrMagnitude > MaxSpeed*MaxSpeed)
+    {
+      rb.velocity = rb.velocity.normalized * MaxSpeed;
+    }
+  }
+}
---
+++ Math362Project1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FollowPlayer : MonoBehaviour {
+
+  GameObject player = null;
+  public float MoveSpeed = 10;
+
+  Rigidbody rb;
+
+  void Start ()
+  {
+    rb = GetComponent<Rigidbody>();
+    var cc = FindObjectOfType<CharacterController>();
+    if (cc)
+      player = cc.gameObject;
+	}
+	
+	void Update ()
+  {
+    if (player == null) return;
+
+    Vector3 dirToPlayer = player.transform.position - gameObject.transform.position;
+    Vector3 moveVector = dirToPlayer.normalized * MoveSpeed;
+
+    rb.AddForce(moveVector);
+  }
+}
